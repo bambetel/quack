4c369cee9e2a83cbda6453f1a2c154b474929256
lukasz <lukasz.mat@gmail.com>
2024-03-02 20:00:48 +0100

    added tag format checking

--- web/js/taginput.js
+++ web/js/taginput.js
@@ -1,13 +1,15 @@
 class TagInput extends HTMLElement {
 	tags = Array("test", "tag", "third");
 	tagList = document.createElement("div");
+	feedback = document.createElement("p");
 
 	constructor() {
 		super();
 
-		let shadow = this.attachShadow({ mode: "open" });
-
+		const shadow = this.attachShadow({ mode: "open" });
 		const label = document.createElement("label");
+		const input = document.createElement("input");
+
 		label.innerText = "Item Tags:";
 		shadow.appendChild(label);
 		shadow.appendChild(this.tagList);
@@ -16,20 +18,40 @@ class TagInput extends HTMLElement {
 			let span = this.createTagNode(elem);
 			this.tagList.appendChild(span);
 		});
-		let input = document.createElement("input");
 
 		input.addEventListener("keypress", (e) => {
 			// TODO all realatable events
 			if (e.key == "Enter") {
+				let msg = "";
+				if (this.tags.includes(input.value)) {
+					this.feedback.innerHTML = "Duplicate tag. ";
+					return;
+				}
 				if (input.value.length < 3) {
+					msg += "Tag should be at least 3 characters long. ";
+				}
+				if (input.value.length > 30) {
+					msg += "Tag should be at most 30 characters long. ";
+				}
+				if (!/^\w+$/.test(input.value)) {
+					msg += "Tag should consist only of letters, numbers and _. ";
+				}
+				if (msg != "") {
+					this.feedback.innerHTML = msg;
 					return;
 				}
 				this.tags.push(input.value);
 				this.tagList.appendChild(this.createTagNode(input.value));
 				input.value = "";
+				this.feedback.innerHTML = "";
+			} else {
+				this.feedback.innerHTML = "";
 			}
 		});
 		shadow.appendChild(input);
+
+		this.feedback.innerHTML = "";
+		shadow.appendChild(this.feedback);
 	}
 
 	createTagNode(tag) {
