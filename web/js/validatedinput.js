554e20de7087ef94905964c3e8d59745703b2287
lukasz <lukasz.mat@gmail.com>
2024-03-02 18:08:35 +0100

    checking out web component API

---
+++ web/js/validatedinput.js
@@ -0,0 +1,40 @@
+class ValidatedInput extends HTMLElement {
+	static observedAttributes = ["value"];
+	label = "Name";
+	value = "Jan";
+	placeholder = "Paweł";
+	name = "name";
+	id = "input-name";
+
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		const shadow = this.attachShadow({ mode: 'closed' });
+
+		shadow.innerHTML = `
+		<div>
+			<label for=${this.id}>${this.label}</label>
+			<input type="text" name="${this.name}" id="${this.id}" value="${this.value}" placeholder="${this.placeholder}">
+			<p><span class="input-feedback">Validation feedback</span></p>
+		</div>
+		`
+
+		let input = shadow.querySelector("input#input-name");
+		let feedback = shadow.querySelector(".input-feedback");
+
+		input.addEventListener("change", (e) => {
+			console.log("You entered:", input.value);
+			feedback.innerHTML = `You entered: ${input.value}`;
+			e.preventDefault();
+		})
+
+	}
+
+	attributesChangeCallback(name, oldValue, newValue) {
+		console.log("Attribute ${name} change: ${oldValue} -> ${newValue}");
+	}
+}
+
+customElements.define("validated-input", ValidatedInput)
