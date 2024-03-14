fc8a2b5ea94f24302d04043d2141f460f61aadba
lukasz <lukasz.mat@gmail.com>
2024-03-14 23:10:10 +0100

    input with lenght check

--- web/js/vi.js
+++ web/js/vi.js
@@ -1,6 +1,7 @@
 class ValidatedInput extends HTMLInputElement {
 	static observedAttributes = ["value"];
-
+	maxLength = 12;
+	minLength = 5;
 
 	constructor() {
 		super();
@@ -12,17 +13,35 @@ class ValidatedInput extends HTMLInputElement {
 		// NOT AVAILABLE HERE
 		// let feedback = document.getElementById(feedbackId);
 
+		this.feedback = document.getElementById(feedbackId);
+
+		this.minLength = parseInt(this.getAttribute('data-minlen'))
+		this.maxLength = parseInt(this.getAttribute('data-maxlen'))
+
 		this.addEventListener("change", (e) => {
-			let feedback = document.getElementById(feedbackId);
 			console.log("You entered:", this.value);
-			feedback.innerHTML = `You entered: ${this.value}`;
+			this.validate();
 			e.preventDefault();
 		});
 
 		this.addEventListener("keypress", (e) => {
+			this.validate();
 			console.log(`You pressed: ${this.value} &lt;- ${e.key}`);
 		});
 
+
+	}
+
+	validate() {
+		const feedbackId = this.id + "-feedback";
+		this.feedback = document.getElementById(feedbackId);
+		let valid = this.value.length >= this.minLength && this.value.length <= this.maxLength;
+		this.setAttribute("aria-invalid", valid ? "false" : "true")
+		if (!valid) {
+			this.feedback.innerHTML = `Invalid length, expects ${this.minLength}-${this.maxLength} characters`;
+		} else {
+			this.feedback.innerHTML = "OK";
+		}
 	}
 
 	attributeChangedCallback(name, oldValue, newValue) {
